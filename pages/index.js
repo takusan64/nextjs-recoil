0a8c1463f2fe6fedce524cf66584e553fcc81ce0
yuya_yamaguchi <hafe1119@yahoo.co.jp>
2022-02-20 23:39:44 +0900

    Recoilの基本動作環境を作成

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { useRecoilState } from "recoil";
+import { todoState } from "../recoil/atom"
+
+export default function Home() {
+  // TodoのAtomを呼び出し
+  const [todoList, setTodoList] = useRecoilState(todoState)
+
+  const [todo, setTodo] = useState("")
+
+  const handleChange = (e) =>{
+    setTodo(e.target.value)
+  }
+
+  // Todoリストを更新する
+  const pushTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo])
+    setTodo("")
+  }
+
+  return (
+    <div>
+      <p>todo list</p>
+      <ul>
+        {todoList.length && todoList.map((v) => (
+          <li>{v}</li>
+        ))}
+      </ul>
+      <div>
+        <input type="text" value={todo} onChange={handleChange} />
+        <button onClick={() => pushTodo(todo)}>
+          add Todo
+        </button>
+      </div>
+    </div>
+  )
+}
